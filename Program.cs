45e9354f8a2ce951fa89aff04a5784700851be19
Savanna Dickie <personal3@SavannaMac.local>
2024-10-23 22:01:18 -0600

    Instructions and Challenge text

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using System.Diagnositcs;
+//Savanna Dickie
+//10/23/2024
+//Lab 6 Typing Trainer
+
+Console.Clear();
+int cursorTop = Console.CursorTop;
+int cursorLeft = Console.CursorLeft;
+Console.WriteLine("Welcome to typing practice!\nA challange message will be provided for you to type.\nClick ENTER to start.");
+Console.ReadLine();
+
+Console.Clear();
+
+string challengeText = " Dont give up what you want most for what you want now.";
+
+Console.WriteLine(challengeText);
+//Console.WriteLine("Type the message above");
+//Console.ReadLine();
+//Console.SetCursorPosition(0, cursorTop -1 && 0, cursorLeft + 4);
+//Console.SetCursorPosition(0, cursorLeft);
+Console.SetCursorPosition(0, Console.CursorTop - 1);
+Console.Write(" ");
+Console.ReadLine();
