434cfbd5b7a18b551a4f6fb959bf50ab27e3fd76
Savanna Dickie <personal3@SavannaMac.local>
2024-10-23 22:51:27 -0600

    Read Input One Character At A Time

--- Program.cs
+++ Program.cs
@@ -5,14 +5,14 @@
 //Lab 6 Typing Trainer
 
 Console.Clear();
-int cursorTop = Console.CursorTop;
-int cursorLeft = Console.CursorLeft;
+//int cursorTop = Console.CursorTop;
+//int cursorLeft = Console.CursorLeft;
 Console.WriteLine("Welcome to typing practice!\nA challange message will be provided for you to type.\nClick ENTER to start.");
 Console.ReadLine();
 
-Console.Clear();
+//Console.Clear();
 
-string challengeText = " Dont give up what you want most for what you want now.";
+string challengeText = "Don't give up what you want most for what you want now.";
 
 Console.WriteLine(challengeText);
 //Console.WriteLine("Type the message above");
@@ -20,5 +20,35 @@ Console.WriteLine(challengeText);
 //Console.SetCursorPosition(0, cursorTop -1 && 0, cursorLeft + 4);
 //Console.SetCursorPosition(0, cursorLeft);
 Console.SetCursorPosition(0, Console.CursorTop - 1);
-Console.Write(" ");
-Console.ReadLine();
+//Console.Write(" ");
+//Console.ReadLine();
+Console.ForegroundColor =ConsoleColor.DarkMagenta;
+
+//char temp;
+
+int correct = 0;
+int incorrect = 0;
+
+//Console.Write("jhlkh ");
+//temp = Console.ReadKey(true).KeyChar;
+//Console.Write("Type the message above: ");
+//char temp = Console.ReadKey(true).KeyChar;
+for(int i = 0; i < challengeText.Length; i++)
+{
+    char temp = Console.ReadKey(true).KeyChar;
+     //temp = Console.ReadKey(true).KeyChar;
+    if (temp == challengeText[i])
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write(challengeText[i]);
+        correct++;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(challengeText[i]);
+        incorrect ++;
+    }
+    //temp = Console.ReadKey(true).KeyChar;
+    
+}
--- obj/Debug/net8.0/lab-6-typing-trainer-SavannaDickie.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-6-typing-trainer-SavannaDickie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-6-typing-trainer-SavannaDickie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a9765bf131f5a41ccb8dcfec13d4d016bee12ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45e9354f8a2ce951fa89aff04a5784700851be19")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-6-typing-trainer-SavannaDickie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-6-typing-trainer-SavannaDickie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
